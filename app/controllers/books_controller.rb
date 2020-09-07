cada116da7b992d818ab84f5544bb05f0029c0a3
Chandra Prakash V <chandraprakashveluchamy@gmail.com>
2020-09-07 20:30:55 +0530

    Added form to add new book related data

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,4 +5,17 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+  def new
+    @book = Book.new
+  end
+  def create
+    @book = Book.new(params.require(:book).permit(:title, :description))
+    if @book.save
+      flash[:notice] = "Book details got added to database!!"
+      redirect_to @book 
+    else
+      render 'new'
+    end 
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   resources :articles
-  resources :books, only: [:show, :index]
+  resources :books, only: [:show, :index, :new, :create]
   root 'pages#home'
   get 'about', to: 'pages#about'
 end
