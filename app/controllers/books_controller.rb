eca2cc3cb86b4482c72b23b68ddf2de58f048629
Chandra Prakash V <chandraprakashveluchamy@gmail.com>
2020-09-07 13:35:08 +0530

    Index for the book table content display

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+  def index
+    @books = Book.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   resources :articles
-  resources :books, only: [:show]
+  resources :books, only: [:show, :index]
   root 'pages#home'
   get 'about', to: 'pages#about'
 end
